c25170405c6f4a949e7c7084f40984941a9df3f8
Unknown <neighlyd@gmail.com>
2018-06-26 16:34:27 -0700

    fixed profile creation on signup

--- profiles/models.py
+++ profiles/models.py
@@ -13,6 +13,7 @@ class Profile(models.Model):
     def create_user_profile(sender, instance, created, **kwargs):
         if created:
             Profile.objects.create(user=instance)
+        instance.profile.save()
 
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
